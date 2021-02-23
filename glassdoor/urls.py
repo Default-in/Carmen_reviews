5bb5f6ac944b19cf3762b6a14077dac12a0194cc
SahilDefault <sahil@getdefault.in>
2021-02-24 00:12:34 +0530

    new template

--- glassdoor/urls.py
+++ glassdoor/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     path('glassdoor/multiplewords', multiplewords),
     path('glassdoor/companiesinfo', companyInfo),
     path('glassdoor/company/<int:pk>', reviewsInfo),
-    path('', Home),
+    path('', table),
     path('test', test),
     path('glassdoor/', testtable),
     path('both', singlewordboth),
