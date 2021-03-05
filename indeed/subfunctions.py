950b07843549f16d98dca062c2f959a03087ef95
SahilDefault <sahil@getdefault.in>
2021-03-05 16:51:07 +0530

    indeed headless

--- indeed/subfunctions.py
+++ indeed/subfunctions.py
@@ -5,17 +5,27 @@ from selenium import webdriver
 user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.83 " \
              "Safari/537.36 "
 
-driver_location = "/usr/bin/chromedriver"
-binary_location = "/usr/bin/google-chrome"
-
 options = webdriver.ChromeOptions()
-# options.add_experimental_option("excludeSwitches", ["enable-automation"])
-# options.add_experimental_option('useAutomationExtension', False)
-options.add_argument('--disable-blink-features=AutomationControlled')
-options.binary_location = binary_location
+options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+options.add_argument("--window-size=1920,1080")
+options.add_argument("--headless")
 options.add_argument(f'user-agent={user_agent}')
+options.add_argument('--disable-dev-shm-usage')
+options.add_argument("--no-sandbox")
 
-driver = webdriver.Chrome(executable_path=driver_location, chrome_options=options)
+driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=options)
+
+# driver_location = "/usr/bin/chromedriver"
+# binary_location = "/usr/bin/google-chrome"
+#
+# options = webdriver.ChromeOptions()
+# # options.add_experimental_option("excludeSwitches", ["enable-automation"])
+# # options.add_experimental_option('useAutomationExtension', False)
+# options.add_argument('--disable-blink-features=AutomationControlled')
+# options.binary_location = binary_location
+# options.add_argument(f'user-agent={user_agent}')
+#
+# driver = webdriver.Chrome(executable_path=driver_location, chrome_options=options)
 driver.implicitly_wait(10)
 
 
@@ -40,6 +50,7 @@ def indeedLogin():
     password = driver.find_element_by_xpath('//*[@id="login-password-input"]')
     for word in 'P@ssw0rd9':
         password.send_keys(word)
-    wait(30)
+    wait(10)
     driver.find_element_by_xpath('//*[@id="login-submit-button"]').click()
     wait(10)
+    print(driver.title)
